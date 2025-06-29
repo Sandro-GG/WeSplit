22aa26f3cdde80d33dee0c484c5fadd624413501
Sandro Gakharia <sandrogakharia@gmail.com>
2025-06-29 18:35:01 +0400

    Add checkAmount text field

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sandro Gakharia on 29.06.25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
